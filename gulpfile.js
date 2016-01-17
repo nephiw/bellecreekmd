f4c7eff240791281385794ca4a5464e3e2f3220b
Nephi Wright <nephiw@gmail.com>
2016-01-17 14:25:50 -0700

    Switch to use less.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 const gulp = require('gulp'),
-  jade = require('gulp-jade');
+  jade = require('gulp-jade'),
+  less = require('gulp-less');
 
-gulp.task('default', ['templates', 'watch']);
+gulp.task('default', ['templates', 'less', 'watch']);
 
 gulp.task('watch', function () {
-  gulp.watch('src/**/*.jade', ['templates'])
+  gulp.watch('src/templates/**/*.jade', ['templates']);
+  gulp.watch('src/less/**/*.less', ['less']);
 });
 
 function logError (error) {
@@ -15,11 +17,18 @@ function logError (error) {
 gulp.task('templates', function() {
   var LOCALS = {};
 
-  return gulp.src('./src/*.jade')
+  return gulp.src('./src/templates/*.jade')
     .pipe(jade({
       locals: LOCALS,
       pretty: true
     }))
     .on('error', logError)
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('./src/less/styles.less')
+    .pipe(less())
+    .on('error', logError)
+    .pipe(gulp.dest('./dist/css'));
 });
