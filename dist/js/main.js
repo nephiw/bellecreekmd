dfc8a0e1888217c89906cd617b600db822325203
Nephi Wright <nephiw@gmail.com>
2016-01-23 15:32:39 -0700

    Start work importing and showing minutes - calculate next meeting

---
+++ dist/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+  'use strict';
+
+  $(function () {
+    $('.next-meeting');
+  });
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 const gulp = require('gulp'),
-  jade = require('gulp-jade'),
-  less = require('gulp-less'),
-  checkCSS = require( 'gulp-check-unused-css' );
+      jade = require('gulp-jade'),
+      less = require('gulp-less'),
+      babel = require('gulp-babel');
 
-gulp.task('default', ['templates', 'less', 'watch']);
+gulp.task('default', ['templates', 'less', 'babel', 'watch']);
 
 gulp.task('watch', function () {
   gulp.watch('src/templates/**/*.jade', ['templates']);
+  gulp.watch('src/templates/**/*.md', ['templates']);
   gulp.watch('src/less/**/*.less', ['less']);
+  gulp.watch('src/js/**/*.js', ['babel']);
 });
 
 function logError (error) {
@@ -16,7 +18,13 @@ function logError (error) {
 }
 
 gulp.task('templates', function() {
-  var LOCALS = {};
+  // loop over all of the files and store their content as strings in the array
+  // insteado of the file name. Then loop over the string in the template.
+  const LOCALS = {
+    config : {
+      minutes: ['2015-01-21.md']
+    }
+  };
 
   return gulp.src('./src/templates/*.jade')
     .pipe(jade({
@@ -32,4 +40,13 @@ gulp.task('less', function () {
     .pipe(less())
     .on('error', logError)
     .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('babel', function () {
+	return gulp.src('src/js/*.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+    .on('error', logError)
+		.pipe(gulp.dest('dist/js'));
 });
