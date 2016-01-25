ccd8b9b0445398cf2f646eede428fb98303ba81e
Nephi Wright <nephiw@gmail.com>
2016-01-24 19:46:51 -0700

    Add copy and clean, start developing minutes page.

--- dist/js/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-  'use strict';
-
-  $(function () {
-    $('.next-meeting');
-  });
-})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,89 @@
 const gulp = require('gulp'),
+      fs = require('fs'),
+      del = require('del'),
       jade = require('gulp-jade'),
       less = require('gulp-less'),
       babel = require('gulp-babel');
 
-gulp.task('default', ['templates', 'less', 'babel', 'watch']);
+gulp.task('default', ['clean', 'templates', 'less', 'babel', 'copy', 'watch']);
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
   gulp.watch('src/templates/**/*.jade', ['templates']);
-  gulp.watch('src/templates/**/*.md', ['templates']);
+  gulp.watch('src/templates/**/*.html', ['templates']);
   gulp.watch('src/less/**/*.less', ['less']);
   gulp.watch('src/js/**/*.js', ['babel']);
 });
 
-function logError (error) {
+function logError(error) {
   console.log(error.toString());
-  this.emit('end');
+  if (this.emit) {
+    this.emit('end');
+  }
+}
+
+function readFiles(dirname, onFileContent) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      logError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          logError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+var jadeLocals = {
+  config: {
+    minutes: [__dirname + '/src/minutes/2015-01-21.md']
+  }
+};
+
+function onFileContent(filename, content) {
+  var data = {};
+  jadeLocals.config.minutes.push({
+    filename: filename,
+    content: content
+  });
 }
 
 gulp.task('templates', function() {
-  // loop over all of the files and store their content as strings in the array
-  // insteado of the file name. Then loop over the string in the template.
-  const LOCALS = {
-    config : {
-      minutes: ['2015-01-21.md']
-    }
-  };
-
   return gulp.src('./src/templates/*.jade')
     .pipe(jade({
-      locals: LOCALS,
+      locals: jadeLocals,
       pretty: true
     }))
     .on('error', logError)
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('less', function () {
+gulp.task('less', function() {
   return gulp.src('./src/less/styles.less')
     .pipe(less())
     .on('error', logError)
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('babel', function () {
-	return gulp.src('src/js/*.js')
-		.pipe(babel({
-			presets: ['es2015']
-		}))
+gulp.task('babel', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .on('error', logError)
-		.pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy', function () {
+  return gulp.src('vendor/**/*')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
 });
