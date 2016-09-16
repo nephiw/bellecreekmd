2cb3dbf09a72e1b536f4f4753911a33a07b9b5a5
Nephi Wright <nephiw@gmail.com>
2016-09-16 17:07:45 -0600

    HACK - Change date of next CEC meeting

--- src/js/minutes.js
+++ src/js/minutes.js
@@ -27,18 +27,22 @@
             'December'
           ];
 
-    let thirdWednesday,
-        month,
+    let month,
         today = new Date(),
         year = today.getFullYear(),
         day = getThirdWednesdayOfMonth(today);
 
-    if (today.getDate() < day || (today.getDate() === day && today.getHours() < MEETING_START_TIME)) {
+    if (today.getMonth() === 8 && today.getFullYear() === 2016) {
       month = MONTH_NAMES[today.getMonth()];
+      day = 27;
     } else {
-      month = MONTH_NAMES[today.getMonth() + 1];
-      today.setMonth(today.getMonth() + 1);
-      day = getThirdWednesdayOfMonth(today);
+      if (today.getDate() < day || (today.getDate() === day && today.getHours() < MEETING_START_TIME)) {
+        month = MONTH_NAMES[today.getMonth()];
+      } else {
+        month = MONTH_NAMES[today.getMonth() + 1];
+        today.setMonth(today.getMonth() + 1);
+        day = getThirdWednesdayOfMonth(today);
+      }
     }
 
     $('.next-meeting').text(`${month} ${day}, ${year}`);
